426c60c6e5da57a1650fc68877074e20b8d9e568
FREZ543 <mad230333@gmail.com>
2024-09-01 17:51:01 +0100

    Update ToDoList.jsx
    a

--- ToDoList.jsx
+++ ToDoList.jsx
@@ -1,4 +1,4 @@
-/*import  { useState } from 'react'
+import  { useState } from 'react'
 export default function ToDoList(){
  const [ tasks , setTasks] = useState(["do my homework","clean my room"])
  const [ newTask , setNewTask] = useState('')
@@ -53,4 +53,4 @@ export default function ToDoList(){
      
     </div>
   )
-}*/
+}
